24156a4fc11e059e591bb3fdb7cc653715c997ee
poornambiga <83727595+poornambiga@users.noreply.github.com>
2021-07-12 14:24:45 +0530

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,6 +2,11 @@
 
 import com.example.Docker
 
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: '6ea27612-6a27-4815-90da-8cf2055a12bf', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t poornambiga/javaapp:jma1.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push poornambiga/javaapp:jma1.0'
+    }
 }
